992b75e8193d582f277524185d90d5522640d285
dravesstorm <dravesstorm@users.github.com>
2020-03-05 23:52:37 +0200

    fileOutput

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -21,7 +21,7 @@ namespace Task1
             Trainer tr_bars = new Trainer("Valverde");
 
             Team bars = new Team("Barselona", tr_bars);
-            TeamService barselonaServices = new TeamService(bars, cw);
+            TeamService barselonaServices = new TeamService(bars, fw);
 
             //добавление футболистов
             barselonaServices.Add(new Footballer("Tolmachev", 32));
@@ -43,12 +43,12 @@ namespace Task1
             barselonaServices.Add(new Footballer("Emelianenko ", 26));
 
             barselonaServices.Show();
-            cw.WriteLine("\nПопытка добавить 12 игрока");
+            fw.WriteLine("\nПопытка добавить 12 игрока");
             barselonaServices.Add(new Footballer("McKinstry ", 32));
-            cw.WriteLine("Игроки старше 30 лет:");
+            fw.WriteLine("Игроки старше 30 лет:");
             barselonaServices.ShowSenior();
             barselonaServices.ShowTrainer();
-            cw.WriteLine("\n\n");
+            fw.WriteLine("\n\n");
 
 
             Trainer tr_real = new Trainer("Zidane");
@@ -56,7 +56,7 @@ namespace Task1
             Team real = new Team("Real-Madrid", tr_real);
 
 
-            TeamService realServices = new TeamService(real, cw);
+            TeamService realServices = new TeamService(real, fw);
 
 
             realServices.Add(new Footballer("Abraham", 32));
@@ -70,20 +70,19 @@ namespace Task1
             realServices.Add(new Footballer("Johnson", 26));
             realServices.Add(new Footballer("Kendall ", 28));
             realServices.Add(new Footballer("Vanderford ", 32));
-
             realServices.Show();
             realServices.ShowTrainer();
-            cw.WriteLine("\n\n");
+            fw.WriteLine("\n\n");
 
             Refery skomina = new Refery("Skomina", Preferences.FirstTeam);
 
             Game FIFA1 = new Game(barselonaServices.MyTeam, realServices.MyTeam, skomina);
-            GameService FIFA1service = new GameService(FIFA1, cw);
+            GameService FIFA1service = new GameService(FIFA1, fw);
 
             FIFA1service.Start();
-            cw.WriteLine();
+            fw.WriteLine();
             FIFA1service.ShowPredictions();
-            cw.WriteLine(); 
+            fw.WriteLine(); 
             FIFA1service.eGoal += FIFA1service.NoticeGoal;
             FIFA1service.Goal(bars);
             FIFA1service.Goal(real);
